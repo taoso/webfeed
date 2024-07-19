1311a287e6bdf60312cab666e0155ef78b0f44dd
涛叔 <hi@taoshu.in>
2024-07-19 21:39:22 +0800

    bump v0.9

--- event.js
+++ event.js
@@ -22,7 +22,7 @@ const handler = async (id) => {
   let popup = "";
   if (feeds.length > 0) {
     await browser.action.setIcon({
-      path: "icons/icon.png",
+      path: { "128": "icons/icon.png" },
       tabId: id
     });
 
