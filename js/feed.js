5aaa30b023883a91210b024d8ccb34b325037564
涛叔 <hi@taoshu.in>
2024-10-30 01:37:49 +0800

    Never filter feed without summary.
        Just let the user to decide whether or not to subscribe.

--- js/feed.js
+++ js/feed.js
@@ -74,7 +74,7 @@ export class AtomFeed extends Feed {
       // FIXME this.entry will be undefined in some case.
       // parse https://pinlyu.com/atom.xml
       // It seems caused by invalid encoding.
-      if (this._entry && this._entry.summary) { this.entries.push(this._entry); }
+      if (this._entry) { this.entries.push(this._entry); }
       this._state = STATE.FEED;
       return;
     }
@@ -165,9 +165,7 @@ export class RssFeed extends Feed {
     }
 
     if (event == "closetag" && content == "item") {
-      if (this._entry && this._entry.summary) {
-        this.entries.push(this._entry);
-      }
+      if (this._entry) { this.entries.push(this._entry); }
       this._state = STATE.FEED;
       return;
     }
